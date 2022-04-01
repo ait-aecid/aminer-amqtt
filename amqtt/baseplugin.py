2ba1e52e1ba1f48d80dc2e18532333f3fca39c28
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at#>
2022-04-01 16:31:34 +0000

    added version 0.9.2

--- amqtt/baseplugin.py
+++ amqtt/baseplugin.py
@@ -1,3 +1,11 @@
 class BasePlugin() :
     def __init__(self):
-        print("BasePlugin loaded")
+        """
+        Constructor for the plugin
+        """
+
+    def decode(self,payload):
+        """
+        This method decodes the payload.
+        @param payload the payload to decode.
+        """
